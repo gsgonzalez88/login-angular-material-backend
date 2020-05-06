f7cbf15336542575a46774ec0a1a54067d328995
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-06 01:32:06 -0300

    creating post endpoint for authentication

--- src/server.js
+++ src/server.js
@@ -10,8 +10,13 @@ app.use(bodyParser.json())
 
 console.log('server runing on port 3000')
 app.post('/api/authenticate', function (req, res) {
-    console.log('req',req.body);
-    return res.send('Hello world');
+    console.log('req',req.body.email);
+    console.log(req.body.password === '1234')
+    if(req.body.email === 'test@domain.com' && req.body.password === '1234'){
+        res.send(true)
+    } else {
+        return res.send(false)
+    };
 });
 
 app.listen(3000);
