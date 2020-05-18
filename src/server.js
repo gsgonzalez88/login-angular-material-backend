8576a100f0a48d973dacf16001f28490010b4d48
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-18 02:12:41 -0300

    debugging installed

--- src/server.js
+++ src/server.js
@@ -4,12 +4,28 @@ const app = express();
 const logger = require('./module/logger')
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
+const startUpDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`app: ${app.get('env')}`);
 
 //Help secure your apps by setting various HTTP headers.
 app.use(helmet());
 
+//Configuration
+
+console.log(`Applicaction Name: ${config.get('name')}
+Mail server: ${config.get('mail.host')}`);
+
 //for logging the req
-app.use(morgan('tiny'))
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    startUpDebugger('morgan enabled');
+}
+//Db work
+dbDebugger('Connected to the database');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
