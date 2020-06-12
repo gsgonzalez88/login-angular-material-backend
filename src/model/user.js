b90c0a1266d8d302bf99b6cce6a33f18a0eca98f
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-06-11 23:49:40 -0300

    added functions for singin and singup, also added lastname to user model

--- src/model/user.js
+++ src/model/user.js
@@ -6,8 +6,14 @@ const User = mongoose.model('User', new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 2,
         maxlength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50,
     },
     email: {
         type: String,
@@ -24,17 +30,16 @@ const User = mongoose.model('User', new mongoose.Schema({
     }
 }).plugin(uniqueValidator));
 
+//use of Joi library to validate the user information.
 function validateUser(User){
     const schema = {
-        name: Joi.string().min(5).max(50).required(),
+        name: Joi.string().min(2).max(50).required(),
+        lastname: Joi.string().min(2).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
         password: Joi.string().min(5).max(1024).required()
     };
-
     return Joi.validate(user, schema);
 }
-
-
 
 exports.User = User;
 exports.validate = validateUser;
